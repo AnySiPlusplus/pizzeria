612d776b16e60fa7620c859bcf52f48b802406b9
AnySiPlusplus <denis1sorokin@gmail.com>
2022-12-28 15:31:11 +0200

    remove unnecussary files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,6 @@
 import $ from 'jquery';
 
 import 'bootstrap';
-import './button_read_more';
-import './button_view_more';
-import './calculator';
-import './delete_account_button';
-import './addresses_forms';
-import './change_email';
-import './change_password';
-import './review_rating_stars';
-import './checkout_form_with_password';
-import './checkout_email_without_password';
-import './checkout_use_billing';
 
 window.jQuery = $;
 window.$ = $;
