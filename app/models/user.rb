26540f738fc8f4aedc9028f1707265a5bda630b5
AnySiPlusplus <denis1sorokin@gmail.com>
2023-02-22 12:23:03 +0200

    add Oauth google

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :omniauthable, omniauth_providers: %i[github facebook]
+         :confirmable, :omniauthable, omniauth_providers: %i[github facebook google_oauth2]
 
   has_one :billing, dependent: :destroy
   has_one :shipping, dependent: :destroy
